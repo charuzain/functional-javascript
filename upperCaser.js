a0b75927b87f37fecc7d58831263a9d5bf2d3bd9
charuzain <charuabhizain@gmail.com>
2022-05-04 21:56:41 -0400

    Convert string to uppercase

--- upperCaser.js
+++ upperCaser.js
@@ -1,3 +1,11 @@
+/* Task
+
+Write a function that takes an input string and returns it uppercased.
+
+## Arguments
+
+input: a String of random words (lorem ipsum) */
+
 const upperCaser = (input) => {
   return input.split(" ").map(elem=>elem.toUpperCase()).join(" ");
 };
